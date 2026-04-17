2c2bd6e6b13cb10ff6b3ece955a4990e45895495
Khristiam <khryztiam@gmail.com>
2026-04-16 21:49:35 -0600

    Remove .history from repository and update .gitignore

--- .history/next.config_20260416170801.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  pageExtensions: ['js', 'jsx'],
-};
-
-module.exports = nextConfig;
--- .history/next.config_20260416181013.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ['js', 'jsx'],
-};
-
-module.exports = nextConfig;
